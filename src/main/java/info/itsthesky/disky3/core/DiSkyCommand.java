f1a5e9dc86a971bc19559b95bd1f04d410fbb862
Sky <livegogo.16334@gmail.com>
2021-08-31 22:07:19 +0200

    :rocket: Deployed a new DiSky command to check various infos

---
+++ src/main/java/info/itsthesky/disky3/core/DiSkyCommand.java
@@ -0,0 +1,93 @@
+package info.itsthesky.disky3.core;
+
+import info.itsthesky.disky3.DiSky;
+import info.itsthesky.disky3.api.bot.Bot;
+import info.itsthesky.disky3.api.bot.BotManager;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Locale;
+
+import static info.itsthesky.disky3.DiSky.*;
+
+public class DiSkyCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
+        if (sender instanceof Player) {
+            sender.sendMessage("This command can only be executed in console!");
+            return false;
+        }
+
+        if (args.length == 0) {
+            sendHelp();
+            return true;
+        }
+
+        switch (args[0]) {
+            case "help":
+                sendHelp();
+                return true;
+            case "list":
+                ln();
+
+                if (BotManager.getLoadedBots().size() == 0) {
+                    warn("There's no loaded bot :(");
+                } else {
+                    warn("Loaded bots ("+BotManager.getLoadedBots().size()+"):");
+                    ln();
+                    for (Bot bot : BotManager.getLoadedBots())
+                        warn("    - " + bot.getName() + " (&6Connected as "+bot.getCore().getSelfUser().getName() + "#" +bot.getCore().getSelfUser().getDiscriminator()+"&e)");
+                }
+
+                ln();
+                return true;
+            case "info":
+
+                if (args.length == 1) {
+                    error("You must specify a bot name! Use &4/disky list &cto show every loaded bots!");
+                    return false;
+                }
+                final String botName = args[1];
+                if (!BotManager.isLoaded(botName)) {
+                    error("The specified bot name doesn't exist (or is not loaded)! Use &4/disky list &cto show every loaded bots!");
+                    return false;
+                }
+                final Bot bot = BotManager.searchFromName(botName);
+
+                ln();
+                log("Bot information about '" + bot.getName() + "':");
+                ln();
+                log("    - Discord Name & Tag: &3" + bot.getCore().getSelfUser().getName() + "#" +bot.getCore().getSelfUser().getDiscriminator());
+                log("    - Discord ID: &3" + bot.getCore().getSelfUser().getId());
+                log("    - Total Guilds: &3" + bot.getCore().getGuilds().size());
+                log("    - Gateway Ping: &3" + bot.getCore().getGatewayPing());
+                log("    - Internal Status: &3" + bot.getCore().getStatus().name().toLowerCase(Locale.ROOT).replaceAll("_", " "));
+                log("    - Intents:");
+                for (GatewayIntent intent : GatewayIntent.values())
+                    log("    " + (
+                            bot.getCore().getGatewayIntents().contains(intent) ? "&a✓ " : "&c☓ "
+                            ) + intent.name().toLowerCase(Locale.ROOT).replaceAll("_", " ")
+                    );
+                ln();
+
+                return true;
+        }
+
+        sendHelp();
+        return true;
+    }
+
+    public static void sendHelp() {
+        ln();
+        success("DiSky v" + DiSky.getInstance().getDescription().getVersion() + " by ItsTheSky - Help List");
+        ln();
+        log("/disky help &7- &3Show this page");
+        log("/disky info <bot name> &7- &3Show information about a loaded bot");
+        log("/disky list &7- &3Show a list of every loaded bot");
+        ln();
+    }
+}
