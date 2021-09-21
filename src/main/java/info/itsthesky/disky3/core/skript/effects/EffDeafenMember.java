0236eb95f669d8e8bb1f68384003bce0923b44a8
Sky <livegogo.16334@gmail.com>
2021-09-21 19:52:55 +0200

    :sparkles: Added deafen & mute member effects

---
+++ src/main/java/info/itsthesky/disky3/core/skript/effects/EffDeafenMember.java
@@ -0,0 +1,65 @@
+package info.itsthesky.disky3.core.skript.effects;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import info.itsthesky.disky3.api.Utils;
+import info.itsthesky.disky3.api.bot.Bot;
+import info.itsthesky.disky3.api.skript.WaiterEffect;
+import net.dv8tion.jda.api.entities.Member;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class EffDeafenMember extends WaiterEffect {
+
+    static {
+        Skript.registerEffect(
+                EffDeafenMember.class,
+                "deafen [the] [member] %member% [(with|using) [bot] %-bot%]",
+                "undeafen [the] [member] %member% [(with|using) [bot] %-bot%]"
+        );
+    }
+
+    private Expression<Member> exprMember;
+    private Expression<Bot> exprBot;
+    private boolean isUnDeafen;
+
+    @Override
+    public void runEffect(@NotNull Event e) {
+        Member member = exprMember.getSingle(e);
+        final Bot bot = exprBot.getSingle(e);
+
+        if (member == null || bot == null) {
+            restart();
+            return;
+        }
+
+        member.getGuild().retrieveMemberById(member.getId()).queue(m -> {
+            m.deafen(!isUnDeafen).queue(this::restart);
+        });
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean debug) {
+        return (isUnDeafen ? "undeafen" : "deafen") + " " + exprMember.toString(e, debug) + " using bot " +exprBot.toString(e, debug);
+    }
+
+    @Override
+    public boolean initEffect(Expression @NotNull [] exprs, int matchedPattern, @NotNull Kleenean isDelayed, SkriptParser.@NotNull ParseResult parseResult) {
+        exprMember = (Expression<Member>) exprs[0];
+
+        exprBot = (Expression<Bot>) exprs[1];
+        if (exprBot == null)
+            exprBot = Utils.defaultToEventValue(exprBot, Bot.class);
+        if (exprBot == null)
+        {
+            Skript.error("Unable to get the bot in a ban effect.");
+            return false;
+        }
+
+        isUnDeafen = matchedPattern == 1;
+        return true;
+    }
+}
---
+++ src/main/java/info/itsthesky/disky3/core/skript/effects/EffMuteMember.java
@@ -0,0 +1,66 @@
+package info.itsthesky.disky3.core.skript.effects;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import info.itsthesky.disky3.DiSky;
+import info.itsthesky.disky3.api.Utils;
+import info.itsthesky.disky3.api.bot.Bot;
+import info.itsthesky.disky3.api.skript.WaiterEffect;
+import net.dv8tion.jda.api.entities.Member;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class EffMuteMember extends WaiterEffect {
+
+    static {
+        Skript.registerEffect(
+                EffMuteMember.class,
+                "mute [the] [member] %member% [(with|using) [bot] %-bot%]",
+                "unmute [the] [member] %member% [(with|using) [bot] %-bot%]"
+        );
+    }
+
+    private Expression<Member> exprMember;
+    private Expression<Bot> exprBot;
+    private boolean isUnMute;
+
+    @Override
+    public void runEffect(@NotNull Event e) {
+        Member member = exprMember.getSingle(e);
+        final Bot bot = exprBot.getSingle(e);
+
+        if (member == null || bot == null) {
+            restart();
+            return;
+        }
+
+        member.getGuild().retrieveMemberById(member.getId()).queue(m -> {
+            m.mute(!isUnMute).queue(this::restart);
+        });
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean debug) {
+        return (isUnMute ? "unmute" : "mute") + " " + exprMember.toString(e, debug) + " using bot " +exprBot.toString(e, debug);
+    }
+
+    @Override
+    public boolean initEffect(Expression @NotNull [] exprs, int matchedPattern, @NotNull Kleenean isDelayed, SkriptParser.@NotNull ParseResult parseResult) {
+        exprMember = (Expression<Member>) exprs[0];
+
+        exprBot = (Expression<Bot>) exprs[1];
+        if (exprBot == null)
+            exprBot = Utils.defaultToEventValue(exprBot, Bot.class);
+        if (exprBot == null)
+        {
+            Skript.error("Unable to get the bot in a ban effect.");
+            return false;
+        }
+
+        isUnMute = matchedPattern == 1;
+        return true;
+    }
+}
