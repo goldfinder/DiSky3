c6ed96c8ff0771f0c47a0adf7d29e58ef1176486
Sky <livegogo.16334@gmail.com>
2021-09-21 20:05:26 +0200

    :zap: Reworked the discord id feature

---
+++ src/main/java/info/itsthesky/disky3/core/skript/ExprID.java
@@ -0,0 +1,61 @@
+package info.itsthesky.disky3.core.skript;
+
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.util.Kleenean;
+import info.itsthesky.disky3.api.Utils;
+import net.dv8tion.jda.api.entities.IMentionable;
+import net.dv8tion.jda.api.entities.ISnowflake;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class ExprID extends SimpleExpression<String> {
+
+    static {
+        Skript.registerExpression(
+                ExprID.class,
+                String.class,
+                ExpressionType.SIMPLE,
+               "[the] [discord] id of [entity] %object%"
+        );
+    }
+
+    private Expression<ISnowflake> exprEntity;
+
+    @Override
+    protected String @NotNull [] get(@NotNull Event e) {
+        try {
+            ISnowflake mention = Utils.verifyVar(e, exprEntity, null);
+            if (mention == null)
+                return new String[0];
+            return new String[]{mention.getId()};
+        } catch (ClassCastException ex) {
+            return new String[0];
+        }
+    }
+
+    @Override
+    public boolean isSingle() {
+        return true;
+    }
+
+    @Override
+    public @NotNull Class<? extends String> getReturnType() {
+        return String.class;
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean debug) {
+        return "discord id of " + exprEntity.toString(e, debug);
+    }
+
+    @Override
+    public boolean init(Expression<?> @NotNull [] exprs, int matchedPattern, @NotNull Kleenean isDelayed, SkriptParser.@NotNull ParseResult parseResult) {
+        exprEntity = (Expression<ISnowflake>) exprs[0];
+        return true;
+    }
+}
