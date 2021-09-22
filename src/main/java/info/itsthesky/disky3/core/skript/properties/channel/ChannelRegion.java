2aafa84e312582f90b82efd608bb829c0f1795b5
Sky <livegogo.16334@gmail.com>
2021-09-22 19:10:01 +0200

    :sparkles: Added channel region property

---
+++ src/main/java/info/itsthesky/disky3/core/skript/properties/channel/ChannelRegion.java
@@ -0,0 +1,73 @@
+package info.itsthesky.disky3.core.skript.properties.channel;
+
+import ch.njol.skript.classes.Changer;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser;
+import ch.njol.util.Kleenean;
+import ch.njol.util.coll.CollectionUtils;
+import info.itsthesky.disky3.DiSky;
+import info.itsthesky.disky3.api.Utils;
+import info.itsthesky.disky3.api.bot.Bot;
+import info.itsthesky.disky3.api.changers.ChangeablePropertyExpression;
+import info.itsthesky.disky3.api.skript.NodeInformation;
+import net.dv8tion.jda.api.Region;
+import net.dv8tion.jda.api.entities.GuildChannel;
+import net.dv8tion.jda.api.entities.VoiceChannel;
+import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class ChannelRegion extends ChangeablePropertyExpression<GuildChannel, Region> {
+
+    static {
+        register(
+                ChannelRegion.class,
+                Region.class,
+                "[discord] region",
+                "channel"
+        );
+    }
+
+    private NodeInformation info;
+
+    @Override
+    public Class<?>[] acceptChange(Changer.ChangeMode mode, boolean diskyChanger) {
+        if (mode == Changer.ChangeMode.SET)
+            return CollectionUtils.array(Number.class);
+        return CollectionUtils.array();
+    }
+
+    @Override
+    public void change(Event e, Object[] delta, Bot bot, Changer.ChangeMode mode) {
+        if (delta == null || delta.length == 0 || delta[0] == null) return;
+        GuildChannel channel = Utils.verifyVar(e, getExpr(), null);
+        final Region value = (Region) delta[0];
+        if (value == null || channel == null) return;
+
+        channel = bot.getCore().getGuildChannelById(channel.getId());
+
+        channel.getManager().setRegion(value).queue(null, ex -> DiSky.exception(ex, info));
+    }
+
+    @Override
+    protected Region @NotNull [] get(@NotNull Event e, GuildChannel @NotNull [] source) {
+        return new Region[] {((VoiceChannel) source[0]).getRegion()};
+    }
+
+    @Override
+    public @NotNull Class<? extends Region> getReturnType() {
+        return Region.class;
+    }
+
+    @Override
+    public @NotNull String toString(@Nullable Event e, boolean debug) {
+        return "region of " + getExpr().toString(e, debug);
+    }
+
+    @Override
+    public boolean init(Expression<?> @NotNull [] exprs, int matchedPattern, @NotNull Kleenean isDelayed, SkriptParser.@NotNull ParseResult parseResult) {
+        setExpr((Expression<? extends GuildChannel>) exprs[0]);
+        info = new NodeInformation();
+        return true;
+    }
+}
