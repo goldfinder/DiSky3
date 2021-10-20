f886a5c3ed900ef8f6e673f6f8151a2252e9e531
Sky <livegogo.16334@gmail.com>
2021-10-20 20:13:48 +0200

    :zap: Forgot the new category action xD

---
+++ src/main/java/info/itsthesky/disky3/core/skript/properties/channels/NewCategoryAction.java
@@ -0,0 +1,32 @@
+package info.itsthesky.disky3.core.skript.properties.channels;
+
+import info.itsthesky.disky3.api.skript.action.NewAction;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.requests.restaction.ChannelAction;
+import org.jetbrains.annotations.NotNull;
+
+public class NewCategoryAction extends NewAction<ChannelAction> {
+
+    static {
+        register(
+                NewCategoryAction.class,
+                ChannelAction.class,
+                "category"
+        );
+    }
+
+    @Override
+    protected ChannelAction create(@NotNull Guild guild) {
+        return guild.createCategory("default channel");
+    }
+
+    @Override
+    public String getNewType() {
+        return "category";
+    }
+
+    @Override
+    public Class<? extends ChannelAction> getReturnType() {
+        return ChannelAction.class;
+    }
+}
