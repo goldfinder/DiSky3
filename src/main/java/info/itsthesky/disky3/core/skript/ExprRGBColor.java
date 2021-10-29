323e05cb35167c1803ce2a8f90972d2e8588ec32
Sky <livegogo.16334@gmail.com>
2021-10-29 19:07:27 +0200

    :sparkles: Added color from Hex expression

--- src/main/java/info/itsthesky/disky3/core/skript/ExprRGBColor.java
+++ src/main/java/info/itsthesky/disky3/core/skript/ExprRGBColor.java
@@ -19,32 +19,51 @@ import info.itsthesky.disky3.api.bot.BotManager;
 import org.bukkit.event.Event;
 import org.jetbrains.annotations.NotNull;
 
+@Name("Color from RGB / HEX")
+@Description({"Get a skript color to use in embed, role, etc...",
+"If SkImage is installed, both hex and rgb syntax will be prefixed by 'skript' to avoid confusion."})
 public class ExprRGBColor extends SimpleExpression<Color> {
 
 	static {
-		if (!ReflectionUtils.classExist("info.itsthesky.SkImage.SkImage")) {
-			Skript.registerExpression(ExprRGBColor.class, Color.class, ExpressionType.SIMPLE,
-					"[the] color from [the] (rgb|red green blue) %number%[ ][,][ ]%number%[ ][,][ ]%number%");
-		}
+		final String prefix = ReflectionUtils.classExist("info.itsthesky.SkImage.SkImage") ? "skript " : "";
+		Skript.registerExpression(ExprRGBColor.class, Color.class, ExpressionType.SIMPLE,
+				"[the] "+prefix+"color from [the] (rgb|red green blue) %number%[ ][,][ ]%number%[ ][,][ ]%number%",
+				"[the] "+prefix+ "color from [the] hex[adecimal] [input] %string%");
 	}
 
 	private Expression<Number> exprRed, exprGreen, exprBlue;
+	private Expression<String> exprHex;
+	private boolean isHex = false;
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public boolean init(Expression<?> @NotNull [] exprs, int matchedPattern, @NotNull Kleenean isDelayed, SkriptParser.@NotNull ParseResult parseResult) {
-		exprRed = (Expression<Number>) exprs[0];
-		exprGreen = (Expression<Number>) exprs[1];
-		exprBlue = (Expression<Number>) exprs[2];
+		isHex = matchedPattern == 1;
+
+		if (isHex) {
+			exprHex = (Expression<String>) exprs[0];
+		} else {
+			exprRed = (Expression<Number>) exprs[0];
+			exprGreen = (Expression<Number>) exprs[1];
+			exprBlue = (Expression<Number>) exprs[2];
+		}
 		return true;
 	}
 
 	@Override
 	protected Color @NotNull [] get(@NotNull Event e) {
-		int red = Utils.verifyVar(e, exprRed, 0).intValue();
-		int green = Utils.verifyVar(e, exprGreen, 0).intValue();
-		int blue = Utils.verifyVar(e, exprBlue, 0).intValue();
-		return new Color[] {new ColorRGB(red, green, blue)};
+		if (isHex) {
+			final String input = exprHex.getSingle(e);
+			if (input == null)
+				return new Color[0];
+			final java.awt.Color color = java.awt.Color.decode(input.contains("#") ? input : "#" + input);
+			return new Color[] {new ColorRGB(color.getRed(), color.getGreen(), color.getBlue())};
+		} else {
+			int red = Utils.verifyVar(e, exprRed, 0).intValue();
+			int green = Utils.verifyVar(e, exprGreen, 0).intValue();
+			int blue = Utils.verifyVar(e, exprBlue, 0).intValue();
+			return new Color[] {new ColorRGB(red, green, blue)};
+		}
 	}
 
 	@Override
@@ -59,7 +78,11 @@ public class ExprRGBColor extends SimpleExpression<Color> {
 
 	@Override
 	public @NotNull String toString(Event e, boolean debug) {
-		return "color from rgb " + exprRed.toString(e, debug) + " " + exprGreen.toString(e, debug) + " " + exprBlue.toString(e, debug);
+		if (isHex) {
+			return "color from hex " + exprHex.toString(e, debug);
+		} else {
+			return "color from rgb " + exprRed.toString(e, debug) + ", " + exprGreen.toString(e, debug) + ", " + exprBlue.toString(e, debug);
+		}
 	}
 
 }
