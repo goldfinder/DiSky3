817941894d2d2dbab8895b645ac1073481dd3bb6
Sky <livegogo.16334@gmail.com>
2021-11-01 15:18:41 +0100

    :bug: Fixed download attachment effect not working

--- src/main/java/info/itsthesky/disky3/core/skript/properties/attachments/EffAttDownload.java
+++ src/main/java/info/itsthesky/disky3/core/skript/properties/attachments/EffAttDownload.java
@@ -10,16 +10,19 @@ import ch.njol.skript.lang.Expression;
 import ch.njol.skript.lang.SkriptParser;
 import ch.njol.util.Kleenean;
 
+import info.itsthesky.disky3.DiSky;
+import info.itsthesky.disky3.api.DiSkyException;
+import info.itsthesky.disky3.api.skript.WaiterEffect;
 import net.dv8tion.jda.api.entities.Message;
 import org.bukkit.event.Event;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
 
 @Name("Download Attachment")
 @Description("Download the specific attachment to a file path.")
 @Examples("download {_attachment} in folder \"plugins/data/attachments/\"")
-@Since("1.7")
-public class EffAttDownload extends Effect {
+public class EffAttDownload extends WaiterEffect {
 
     static {
         Skript.registerEffect(EffAttDownload.class,
@@ -31,25 +34,35 @@ public class EffAttDownload extends Effect {
 
     @SuppressWarnings("unchecked")
     @Override
-    public boolean init(Expression<?>[] exprs, int matchedPattern, Kleenean isDelayed, SkriptParser.ParseResult parseResult) {
+    public boolean initEffect(Expression @NotNull [] exprs, int matchedPattern, @NotNull Kleenean isDelayed, SkriptParser.@NotNull ParseResult parseResult) {
         this.exprAtt = (Expression<Message.Attachment>) exprs[0];
         this.exprPath = (Expression<String>) exprs[1];
         return true;
     }
 
     @Override
-    protected void execute(Event e) {
+    public void runEffect(@NotNull Event e) {
         Message.Attachment attachment = exprAtt.getSingle(e);
         String path = exprPath.getSingle(e);
         if (attachment == null || path == null) return;
         File file = new File(path);
-        if (!file.isDirectory()) return;
-        file.mkdirs();
-        attachment.downloadToFile(file);
+        if (file.isDirectory()) {
+            DiSky.exception(new DiSkyException("DiSky tried to download an attachment, but cannot since the specified path is a folder and not a file."), getNode());
+            return;
+        }
+
+        attachment.downloadToFile(file)
+                .thenAccept(this::restart)
+                .exceptionally(t ->
+                {
+                    DiSky.exception(t, getNode());
+                    restart();
+                    return null;
+                });
     }
 
     @Override
-    public String toString(Event e, boolean debug) {
+    public @NotNull String toString(Event e, boolean debug) {
         return "download attachment " + exprAtt.toString(e, debug) + " to folder " + exprPath.toString(e, debug);
     }
 
