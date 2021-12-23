8e37a33030bf9828e7bf713daf8e4d360dde45a0
wallace4BR <wallaceinacio02@gmail.com>
2021-12-23 19:42:37 -0300

    Added skript event to Bot Load

--- src/main/java/info/itsthesky/disky3/core/events/bot/BotLoad.java
+++ src/main/java/info/itsthesky/disky3/core/events/bot/BotLoad.java
@@ -6,12 +6,19 @@ import info.itsthesky.disky3.api.bot.Bot;
 import info.itsthesky.disky3.api.bot.BotManager;
 import info.itsthesky.disky3.api.skript.events.DiSkyEvent;
 import info.itsthesky.disky3.api.skript.events.SimpleDiSkyEvent;
+import info.itsthesky.disky3.core.events.guild.GuildAFKChannel;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import org.jetbrains.annotations.NotNull;
 
 public class BotLoad extends DiSkyEvent<ReadyEvent> {
 
     static {
+        DiSkyEvent.register("Bot Load", GuildAFKChannel.class, GuildAFKChannel.EvtGuildAFKChannel.class,
+                        "[discord] bot (ready|load)")
+                .setName("Bot Load")
+                .setDesc("Fired when a bot finishes loading in the server.")
+                .setExample("on bot load:");
+
         EventValues.registerEventValue(EvtBotLoad.class, Bot.class, new Getter<Bot, EvtBotLoad>() {
             @Override
             public Bot get(@NotNull EvtBotLoad event) {
